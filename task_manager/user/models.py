cb6c576e488878ad693b75ef2f32e983cb5dd8bd
Дубинчик Андрей <evil_ghost@bk.ru>
2021-05-20 00:04:26 +0300

    kill migrations

--- task_manager/user/models.py
+++ task_manager/user/models.py
@@ -1,5 +1,13 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
-    pass
+    first_name = models.CharField(_('first name'), max_length=150)
+    last_name = models.CharField(_('last name'), max_length=150)
+    email = models.EmailField(_('email address'))
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.last_name
